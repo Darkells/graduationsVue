53b15f1a766a43ec951dfed3ccd25d9511543396
951086294@qq.com <951086294@qq.com>
2020-02-03 17:03:45 +0800

    axios异步登陆完成，准备接受token处理

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+import axios from 'axios'
+import qs from 'qs'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8088/'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+Vue.prototype.$http = axios
+Vue.prototype.qs = qs
 
 Vue.config.productionTip = false
 
